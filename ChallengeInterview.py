71cb2ba5aaa76879da4433f1cbe52fd2e1d77ca9
viet <37694614+quocvietphung@users.noreply.github.com>
2021-06-01 20:36:24 +0200

    init project

---
+++ ChallengeInterview.py
@@ -0,0 +1,97 @@
+# ********************************************************************************
+# FILE: ChallengeInterview.py                                                    *
+# DESCRIPTION: The program that solves 2 problems FizzBuzz and Counting Ways     *
+# by using the multiple choice                                                   *
+# Author: Quoc Viet Phung                                                        *
+# ********************************************************************************
+
+import sys as System
+from prettytable import PrettyTable
+
+def fizzBuzz(number):
+
+    board = []
+
+    for i in range(1,number+1):
+
+        if i % 3 == 0 and i % 5 == 0:
+            board.append("I'm FizzBuzz")
+
+        elif i % 3 == 0:
+            board.append("I'm Fizz")
+
+        elif i % 5 == 0:
+            board.append("I'm Buzz")
+
+        else:
+            board.append(str(i))
+
+    return board
+
+def countWays(candies):
+    countingWays = [0 for i in range(0, candies + 1)]
+
+    # base cases
+    countingWays[0] = countingWays[1] = 1
+    countingWays[2] = 2
+
+    # Iterate for all values from 3 to n
+    for i in range(3, candies + 1):
+        countingWays[i] = countingWays[i - 1] + countingWays[i - 2] + countingWays[i - 3]
+
+    return countingWays[candies]
+
+# Display program FizzBuzz
+def displayFizzBuzz() :
+    print("    Welcome to FizzBuzz!\n")
+    number = int(input("    Enter number of FizzBuzz : "))
+    print("\n    Board FizzBuzz with" , number , "numbers input : \n")
+    print(fizzBuzz(number))
+    print("\n\t\t\t\t_______________________________________________________________________________________________________\n")
+    print("\n\t\t\t\t\t\tGame over, would you like to play FizzBuzz with other numbers? \n")
+
+# Display program Counting Ways Candies
+def displaycountingWays() :
+    print("    Welcome to CountWaysCandies!\n")
+    candies = int(input("    Enter number of candies : "))
+    print()
+    table = PrettyTable(['Number of candies', 'Number of the ways counting candies'])
+    table.add_row([candies, countWays(candies)])
+    print(table)
+    print("\n\t\t\t\t_______________________________________________________________________________________________________\n")
+    print("\n\t\t\t\t\t\tGame over, would you like to count ways with other numbers of candies? \n")
+
+def menu():
+    print()
+    print("*********************************** Welcome to Challenge Interview ***********************************")
+    choice = input("""
+    1: FizzBuzz
+    2: Counting ways of candies  
+      
+    Hint: Press 1 or 2 to run FizzBuzz or Counting ways of candies. If you want to quit, type quit or q. 
+      
+    Please enter your choice: """)
+
+    if choice == "1" :
+        print("\n    ******************************************************************\n")
+        displayFizzBuzz()
+        menu()
+    elif choice == "2" :
+        print("\n    ******************************************************************\n")
+        displaycountingWays()
+        menu()
+    elif choice == "quit" or choice == "q":
+        print("\n    Thank you, see you again! \n\n******************************************************************\n")
+        System.exit
+        print()
+    else:
+        print()
+        print("    Warning: You must only select either 1 or 2 !")
+        print("    Please try again! ")
+        menu()
+
+def main():
+    menu()
+
+#Start program
+main()
---
+++ Readme.md
@@ -0,0 +1,50 @@
+Challenge Interview 
+===================
+
+Author : Quoc Viet Phung 
+
+## Introduction 
+<br/>
+A small project to solve 2 algorithm challenges about FizzBuzz and CountingWaysCandies
+
+## How to run program 
+
+1. Open folder ChallengeInterview
+
+    ```sh
+    $ cd ChallengeInterview 
+    ```
+
+2. Install prettytable 
+   
+    ```sh
+    $ pip3 install prettytable
+    ```
+   
+3. Run program ChallengeInterview 
+
+    ```sh
+    $ python ChallengeInterview.py
+    ```
+
+
+##Display
+1. Install prettytable
+![PrettyTable](display/prettytable.png)
+2. Start program
+![Start](display/start.png)
+3. Run FizzBuzz 
+![FizzBuzz](display/fizzbuzz.png)
+4. Run CountWays 
+![CountWays](display/countways.png)
+5. Exit programm 
+![Exit](display/quit.png)
+
+
+
+
+
+   
+   
+   
+
