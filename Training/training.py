f4dc801e619a2b1eaf0f36a4ae1d36570b18a937
viet <37694614+quocvietphung@users.noreply.github.com>
2022-05-18 12:17:04 +0200

    Install tensorflow , create traning

---
+++ Training/training.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_val, y_val = X_train[50000:60000,:], y_train[50000:60000]
+X_train, y_train = X_train[:50000,:], y_train[:50000]
+print(X_train.shape)
